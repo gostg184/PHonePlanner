1e857a322a648d53a869322f06a6e9be04a3fc02
gostg184 <gostg184@gmail.com>
2026-02-11 11:13:43 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Demo PHone Planner</title>
+	<title>Demo Phone Planner</title>
 	<!-- UIKit CSS -->
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.15.22/dist/css/uikit.min.css" />
 	<!-- UIKit JS -->
